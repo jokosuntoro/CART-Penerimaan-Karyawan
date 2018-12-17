46119a6a03d33b240870123d16a1c0282d9f9b40
Joko Suntoro <jokosunt@gmail.com>
2018-12-17 20:39:09 +0700

    Update cart.php

--- public/cart.php
+++ public/cart.php
@@ -1,14 +1,22 @@
 <?php
 require '../vendor/autoload.php';
-use jokodm\Datamining\Klasifikasi\DecisionTree;
-use jokodm\Datamining\Dataset\Demo\PenerimaanPekerja;
+use jokodm\Datamining\Klasifikasi\DecisionTree; //ubah
+use jokodm\Datamining\Dataset\Demo\PenerimaanPekerja; //ubah
 
-$dataset = new PenerimaanPekerja();
+$dataset = new PenerimaanPekerja(); //ubah
 
-$klasifikasi = new DecisionTree();
+$klasifikasi = new DecisionTree(); //ubah
 $klasifikasi->train($dataset->getSamples(),$dataset->getTargets());
+
+$txtNama = $_POST['txtNama']; //ubah
+$numUsia = $_POST['numUsia']; //ubah
+$status = $_POST['status']; //ubah
+$test = $_POST['tes']; //ubah
+$sosial = $_POST['sosial']; //ubah
+$kinerja = $_POST['kinerja']; //ubah
+
 //Status,Test Tertulis,Hubungan Sosial,Kinerja
-$pred = $klasifikasi->predict(['Lajang', 'Cukup', 'Buruk', 'Baik']);
+$pred = $klasifikasi->predict([$status, $test, $sosial, $kinerja]); //ubah
 
 if ($pred == "Ya") {
 	echo "Diterima bekerja";
